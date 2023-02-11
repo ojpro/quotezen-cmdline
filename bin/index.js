1f24406128a2cb679cbfea66d13ed8365b9e9f96
ojpro <contact@ojpro.me>
2023-02-11 23:36:57 +0100

    🌹 created a beautiful quote box

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,43 @@
 #!/usr/bin/env node
 
-console.log("Random quote")
+import chalk from "chalk";
+import boxen from "boxen";
+
+let quote = chalk.white.bold("To do great things is difficult; but to command great things is more difficult.!");
+const author = chalk.white.bold.italic.dim("\n\n- Unknown")
+
+quote += author;
+
+const title = chalk.white.bold("QuoteZen")
+const leftQuotation = chalk.blueBright.bold("“")
+const rightQuotation = chalk.blueBright.bold("”")
+
+const boxenOptions = {
+    title: title,
+    titleAlignment: "center",
+    borderStyle: {
+        topLeft: leftQuotation,
+        topRight: '╮',
+        bottomLeft: '╰',
+        bottomRight: rightQuotation,
+        top: '─',
+        bottom: '─',
+        left: '│',
+        right: '│'
+    },
+    borderColor: "gray",
+    align:"center",
+    width:100,
+    padding:{
+        top: 2,
+        bottom: 1,
+    },
+    margin:4,
+    float:"center",
+    textAlignment:"center"
+
+};
+
+const quoteBox = boxen(quote, boxenOptions);
+
+console.log(quoteBox)
