23d872cc854aee68ce7107dd5ec8872aab9a428a
ojpro <contact@ojpro.me>
2023-02-12 00:12:49 +0100

    🔥 Created the first Version

--- bin/index.js
+++ bin/index.js
@@ -2,42 +2,63 @@
 
 import chalk from "chalk";
 import boxen from "boxen";
+import axios from "axios";
+import dotenv from "dotenv";
 
-let quote = chalk.white.bold("To do great things is difficult; but to command great things is more difficult.!");
-const author = chalk.white.bold.italic.dim("\n\n- Unknown")
+// load dotenv configs
+dotenv.config()
 
-quote += author;
+// styling of the quote box
+function displayQuote(quoteText, quoteAuthor) {
+    let quote = chalk.white.bold(quoteText);
+    const author = chalk.white.bold.italic.dim(`\n\n - ${quoteAuthor}`)
+    quote += author;
 
-const title = chalk.white.bold("QuoteZen")
-const leftQuotation = chalk.blueBright.bold("“")
-const rightQuotation = chalk.blueBright.bold("”")
+    const title = chalk.white.bold("QuoteZen")
+    const leftQuotation = chalk.blueBright.bold("“")
+    const rightQuotation = chalk.blueBright.bold("”")
 
-const boxenOptions = {
-    title: title,
-    titleAlignment: "center",
-    borderStyle: {
-        topLeft: leftQuotation,
-        topRight: '╮',
-        bottomLeft: '╰',
-        bottomRight: rightQuotation,
-        top: '─',
-        bottom: '─',
-        left: '│',
-        right: '│'
-    },
-    borderColor: "gray",
-    align:"center",
-    width:100,
-    padding:{
-        top: 2,
-        bottom: 1,
-    },
-    margin:4,
-    float:"center",
-    textAlignment:"center"
+    const boxenOptions = {
+        title: title,
+        titleAlignment: "center",
+        borderStyle: {
+            topLeft: leftQuotation,
+            topRight: '╮',
+            bottomLeft: '╰',
+            bottomRight: rightQuotation,
+            top: '─',
+            bottom: '─',
+            left: '│',
+            right: '│'
+        },
+        borderColor: "gray",
+        align: "center",
+        width: 100,
+        padding: {
+            top: 2,
+            bottom: 1,
+        },
+        margin: 4,
+        float: "center",
+        textAlignment: "center"
 
-};
+    };
 
-const quoteBox = boxen(quote, boxenOptions);
+    const quoteBox = boxen(quote, boxenOptions);
 
-console.log(quoteBox)
+    console.log(quoteBox)
+}
+
+// fetch a random quote
+async function fetchRandomQuote() {
+    await axios
+        .get("https://quotezen.vercel.app/api/quotes/random")
+        .then((response) => {
+            let quote = response.data
+
+            displayQuote(quote.quote, quote.author)
+        })
+        .catch((err) => console.log(err));
+}
+
+await fetchRandomQuote()
